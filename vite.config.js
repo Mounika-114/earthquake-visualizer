c3f36f9c997c8b1685fc0d744ea3b03e08c6aee5
Marri Mounika <marrimounika114@gmail.com>
2025-09-03 18:03:11 +0000

    change the files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: [".csb.app"], // allow all CodeSandbox preview hosts
+    host: true, // bind to all interfaces
+    port: 5173,
+  },
+});
